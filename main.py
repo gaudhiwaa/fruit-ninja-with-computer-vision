414725f8e97eb06f7c881e0d926fe0197458f529
Gaudhiwaa Hendrasto <88303669+gaudhiwaa@users.noreply.github.com>
2024-03-09 20:56:53 +0700

    feat: add slicing effect and fix session_duratio bug

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 from hand_detector import HandDetector
 from fruit import Fruit
 from constants import screen_height, screen_width, screen, clock, white, red, blue, black
+import random
+
 
 def main():
     prev_time = 0
@@ -13,10 +15,15 @@ def main():
     handDetector = HandDetector()
     cap = cv2.VideoCapture(0)
     score = 0
-    fruit_images = ["apple", "banana", "orange"]  # Add more fruit images as needed
+    line_width = 0
+    prev_tip_x = 0
+    prev_tip_pos = (0, 0)
+
+    # Add more fruit images as needed
+    fruit_images = ["apple", "banana", "orange"]
 
     # Session settings
-    session_duration = 8 # 4 seconds per session
+    session_duration = 2  # 4 seconds per session
     session_start_time = time.time()
     fruits_dropped = 0
     max_fruits_per_session = 5  # Maximum 4 fruits per session
@@ -24,26 +31,33 @@ def main():
     # List to store active fruits
     active_fruits = []
 
-     # Counter for fruits that are not colliding and are outside of the screen
+    # Counter for fruits that are not colliding and are outside of the screen
     fruits_not_colliding = 0
 
     # Initialize finger coordinates
     index_finger_tip_x = 0
     index_finger_tip_y = 0
-    
+
     # Display "Start Game" text
     start_font = pygame.font.Font(None, 36)
     start_text = start_font.render("Start Game", True, white)
     subtitle_font = pygame.font.Font(None, 24)
-    subtitle_text = subtitle_font.render("Ensure good lighting—neither too dark nor too bright.", True, white)
+    subtitle_text = subtitle_font.render(
+        "Ensure good lighting—neither too dark nor too bright.", True, white)
     subtitle_font_2 = pygame.font.Font(None, 24)
-    subtitle_text_2 = subtitle_font_2.render("Make sure your hand is positioned approximately 30 cm from the webcam.", True, white)
+    subtitle_text_2 = subtitle_font_2.render(
+        "Make sure your hand is positioned approximately 30 cm from the webcam.", True, white)
     subtitle_font_3 = pygame.font.Font(None, 24)
-    subtitle_text_3 = subtitle_font_2.render("Press the spacebar to begin the game.", True, white)
-    start_rect = start_text.get_rect(center=(screen_width // 2, screen_height // 2 - 40))
-    subtitle_rect = subtitle_text.get_rect(center=(screen_width // 2, screen_height // 2))
-    subtitle_rect_2 = subtitle_text_2.get_rect(center=(screen_width // 2, screen_height // 2 + 20))
-    subtitle_rect_3 = subtitle_text_3.get_rect(center=(screen_width // 2, screen_height // 2 + 40))
+    subtitle_text_3 = subtitle_font_2.render(
+        "Press the spacebar to begin the game.", True, white)
+    start_rect = start_text.get_rect(
+        center=(screen_width // 2, screen_height // 2 - 40))
+    subtitle_rect = subtitle_text.get_rect(
+        center=(screen_width // 2, screen_height // 2))
+    subtitle_rect_2 = subtitle_text_2.get_rect(
+        center=(screen_width // 2, screen_height // 2 + 20))
+    subtitle_rect_3 = subtitle_text_3.get_rect(
+        center=(screen_width // 2, screen_height // 2 + 40))
     show_start_text = True
 
     # Dictionary to track active animations and their start times
@@ -55,7 +69,8 @@ def main():
     for fruit_name in fruit_images:
         animation_image_path = f"Assets/FruitSliced/{fruit_name}_splash.png"
         animation_images[fruit_name] = pygame.image.load(animation_image_path)
-        animation_images[fruit_name] = pygame.transform.scale(animation_images[fruit_name], (80, 80))
+        animation_images[fruit_name] = pygame.transform.scale(
+            animation_images[fruit_name], (80, 80))
 
     while True:
         for event in pygame.event.get():
@@ -83,7 +98,7 @@ def main():
             # Reset session settings
             session_start_time = time.time()
             fruits_dropped = 0
-            active_fruits.clear()
+            # active_fruits.clear()
 
         # Read video frame from the webcam
         _, img = cap.read()
@@ -112,7 +127,8 @@ def main():
         # Convert image to Pygame format
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img_pygame = pygame.surfarray.make_surface(np.rot90(img_rgb))
-        img_pygame = pygame.transform.scale(img_pygame, (screen_width, screen_height))
+        img_pygame = pygame.transform.scale(
+            img_pygame, (screen_width, screen_height))
 
         # Draw active fruits and show the image in the Pygame window
         screen.blit(img_pygame, (0, 0))
@@ -120,33 +136,53 @@ def main():
             fruit.draw(screen)
             # Check if the fruit is outside the window and hasn't collided
             if not hasattr(fruit, 'collided') and (
-                fruit.rect.x == 0 or 
+                fruit.rect.x == 0 or
                 fruit.rect.x == screen_width or
                 fruit.rect.y == 0 or
                 fruit.rect.y == screen_height
             ):
                 fruits_not_colliding += 1  # Increment the counter for non-colliding fruits
                 # screen.fill(black)
-                
+
             # Display fruit coordinates
             font = pygame.font.Font(None, 24)
-            text = font.render(f"{fruit.rect.x}, {fruit.rect.y}", True, (255, 255, 255))
+            text = font.render(
+                f"{fruit.rect.x}, {fruit.rect.y}", True, (255, 255, 255))
             screen.blit(text, (fruit.rect.x, fruit.rect.y - 30))
 
-        # Display index finger coordinates
         if len(landmarks_list) > 0:
             index_finger_tip_x = landmarks_list[8][1]
             index_finger_tip_y = landmarks_list[8][2]
             # Draw a green dot at the index finger tip position
-            pygame.draw.circle(screen, (0, 255, 0), (screen_width-index_finger_tip_x, index_finger_tip_y), 15)
+            pygame.draw.circle(
+                screen, (0, 255, 0), (screen_width - index_finger_tip_x, index_finger_tip_y), 15)
+
+            # Draw a white line with sharp edges if the finger tip position has changed significantly
+            cur_tip_pos = (screen_width - index_finger_tip_x,
+                           index_finger_tip_y)
+
+            # line_width = abs(index_finger_tip_x - prev_tip_x)
+
+            # adjust threshold as needed
             
+            # Calculate the width of the polygon
+
+                # if np.linalg.norm(np.array(cur_tip_pos) - np.array(prev_tip_pos)) > 7:
+                # Randomize the offset
+            offset = random.choice([3, 5, 7])
+                # Draw a triangle to create sharp edges
+            pygame.draw.polygon(screen, white, [
+                                    prev_tip_pos, cur_tip_pos, (cur_tip_pos[0] + offset, cur_tip_pos[1] + offset)])
+            prev_tip_pos = cur_tip_pos
+
         font = pygame.font.Font(None, 24)
-        text = font.render(f"Not collide: {fruits_not_colliding}", True, (255, 255, 255))
+        text = font.render(
+            f"Not collide: {fruits_not_colliding}", True, (255, 255, 255))
         screen.blit(text, (10, 10))
 
         # Display the score
         font = pygame.font.Font(None, 36)
-        score_text = font.render(f"Score: {score}", True, white)
+        score_text = font.render(f"Score: {line_width}", True, white)
         score_rect = score_text.get_rect(topright=(screen_width - 10, 10))
         screen.blit(score_text, score_rect)
 
@@ -175,23 +211,23 @@ def main():
                 if collision:
                     # Load half fruit image based on fruit name
                     half_fruit_image_path = f"Assets/HalfFruits/{fruit.name}_half_{random.choice(['1', '2'])}.png"
-                    
+
                     # Load animation image based on fruit name
                     half_fruit_animation_path = f"Assets/FruitSliced/{fruit.name}_splash.png"
-                    
+
                     fruit.image = pygame.image.load(half_fruit_image_path)
                     fruit.image = pygame.transform.scale(fruit.image, (80, 80))
-                    
+
                     # Start animation effect
                     active_animations[fruit] = time.time()
-                    
+
                     # Increase the score
                     score += 1
-        
+
         # Update active animations
         for fruit, animation_start_time in list(active_animations.items()):
-            animation_duration = 0.9  # Adjust duration as needed
-            
+            animation_duration = 3  # Adjust duration as needed
+
             if time.time() - animation_start_time < animation_duration:
                 # Load animation image and draw it
                 fruit_name = fruit.name
@@ -210,6 +246,7 @@ def main():
                 return
 
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
